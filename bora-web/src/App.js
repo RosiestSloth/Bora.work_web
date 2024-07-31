92f7dce67327170f6bc927cc440adfd6900eba8f
RosiestSloth <165097853+RosiestSloth@users.noreply.github.com>
2024-07-31 20:23:34 -0300

    Atualização da tela inicial
    Aqui eu configuro alguns aspectos da tela inicial do site do Bora!

--- bora-web/src/App.js
+++ bora-web/src/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const teste = () => {
-  window.open('https://www.youtube.com')
-}
+function linkBora () {
+  window.open('https://bora.work/map', '_blank');
+};
 
-function MeuBotão() {
+//Pega as horas atuais do servidor e escreve no HTML
+
+const Horas = () => {
+  const horas = new Date().getHours().toString()
+  const minutos = new Date().getMinutes()
   return (
-    <button onClick={teste} className='text-white bg-gray rounded-8 w-10 hover:bg-black active:bg-white active:text-black h-9 relative m-auto'
-    >
-      Click Me
-    </button>
-  );
+    <p className='text-white mr-4'>
+      {horas}:{minutos}
+    </p>
+  )
 }
 
 export default function App() {
   return (
-    <main className='bg-white h-20 absolute size-'>
-      <MeuBotão />
-
+    <main>
+      <header className='backdrop-blur-lg bg-white/10 border-white/20 rounded-8 p-1 m-4 shadow- shadow-black'>
+        <nav className='flex items-center m-2'>
+          <button onClick={linkBora} className='mr-auto hover:bg-white/10 shadow-mg
+          d hover:shadow-black rounded-8'>
+            <img className='size-10' src='/logo-bora.png' alt='Logo do bora'/>
+          </button >
+            <Horas />
+          <button className='text-white/50 hover:text-white hover:bg-white/10 rounded-8'>
+            <p>
+              Explorar
+            </p>
+            <img src='/seta-link.svg' className='size-4'/>
+          </button>
+        </nav>
+      </header>
+      <section>
+        <div className=''>
+          <h2 className=''>
+            Eventos encantadores começam aqui.
+          </h2>
+          <p>
+            Crie uma página de evento, convide amigos e venda ingressos. Organize um evento memorável hoje.
+          </p>
+          <button>
+          
+          </button>
+        </div>
+        <video autoplay muted loop typeof='video/webm' src='/phone-dark.webm' className='size-80 ml-auto mr-3/6 mt-20'/>
+      </section>
     </main>
   );
 }
